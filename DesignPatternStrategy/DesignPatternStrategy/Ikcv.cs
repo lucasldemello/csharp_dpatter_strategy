b76a673ee2e118e730f63d7aced25cc532fbffa0
Lucas Melo Pereira <lucasluandemelo@gmail.com>
2019-07-02 00:10:30 -0300

    Template Method

--- DesignPatternStrategy/DesignPatternStrategy/Ikcv.cs
+++ DesignPatternStrategy/DesignPatternStrategy/Ikcv.cs
@@ -7,17 +7,17 @@ namespace DesignPatternStrategy
 {
     public class Ikcv : TemplateDeImpostoCondicional
     {
-        override public bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+        public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
         {
             return orcamento.Valor > 500 && temItemMaiorQue100ReaisNo(orcamento);
         }
 
-        override public double MaximaTaxacao(Orcamento orcamento)
+        public override double MaximaTaxacao(Orcamento orcamento)
         {
             return orcamento.Valor * 0.10;
         }
 
-        override public double MinimaTaxacao(Orcamento orcamento)
+        public override double MinimaTaxacao(Orcamento orcamento)
         {
             return orcamento.Valor * 0.06;
         }
